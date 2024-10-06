79b68417e3a476398df7b82193dd4b00acfcf2dd
Manish Jadhav <manishsj289@gmail.com>
2024-10-06 20:18:55 +0530

    featured room section completed

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,18 +1,18 @@
-// import FeaturedRoom from '@/components/FeaturedRoom/FeaturedRoom';
+import FeaturedRoom from '@/components/FeaturedRoom/FeaturedRoom';
 import Gallery from '@/components/Gallery/Gallery';
 import HeroSection from '@/components/HeroSection/HeroSection';
 import NewsLetter from '@/components/NewsLetter/NewsLetter';
 import PageSearch from '@/components/PageSearch/PageSearch';
-// import { getFeaturedRoom } from '@/libs/apis';
+import { getFeaturedRoom } from '@/libs/apis';
 
 const Home = async () => {
-  // const featuredRoom = await getFeaturedRoom();
+  const featuredRoom = await getFeaturedRoom();
 
   return (
     <>
       <HeroSection />
       <PageSearch />
-      {/* <FeaturedRoom featuredRoom={featuredRoom} /> */}
+      <FeaturedRoom featuredRoom={featuredRoom} />
       <Gallery />
       <NewsLetter />
     </>
---
+++ src/libs/apis.ts
@@ -0,0 +1,13 @@
+import { Room } from "@/app/models/room";
+import sanityClient from "./sanity";
+import * as  queries from './sanityQueries';
+
+export async function getFeaturedRoom(){
+  const result = await sanityClient.fetch<Room>(
+    queries.getFeaturedRoomQuery,
+    {},
+    { cache: 'no-cache'}
+  );
+
+  return result;
+}
