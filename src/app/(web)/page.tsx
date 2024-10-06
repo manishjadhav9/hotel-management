8eef783761c2efe7cd04cbf45aab3f85d21835b7
Manish Jadhav <manishsj289@gmail.com>
2024-10-06 18:51:44 +0530

    Search feature implemented

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,9 +1,20 @@
-import HeroSection from "@/components/HeroSection/HeroSection";
+// import FeaturedRoom from '@/components/FeaturedRoom/FeaturedRoom';
+// import Gallery from '@/components/Gallery/Gallery';
+import HeroSection from '@/components/HeroSection/HeroSection';
+// import NewsLetter from '@/components/NewsLetter/NewsLetter';
+import PageSearch from '@/components/PageSearch/PageSearch';
+// import { getFeaturedRoom } from '@/libs/apis';
 
-const Home = () => {
+const Home = async () => {
+  // const featuredRoom = await getFeaturedRoom();
+
   return (
     <>
       <HeroSection />
+      <PageSearch />
+      {/* <FeaturedRoom featuredRoom={featuredRoom} />
+      <Gallery />
+      <NewsLetter /> */}
     </>
   );
 };
