1f42a335b7c4b3586bf539b1c9259b92c730b592
Manish Jadhav <manishsj289@gmail.com>
2024-10-06 19:01:12 +0530

    Newsletter section completed

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,7 +1,7 @@
 // import FeaturedRoom from '@/components/FeaturedRoom/FeaturedRoom';
 import Gallery from '@/components/Gallery/Gallery';
 import HeroSection from '@/components/HeroSection/HeroSection';
-// import NewsLetter from '@/components/NewsLetter/NewsLetter';
+import NewsLetter from '@/components/NewsLetter/NewsLetter';
 import PageSearch from '@/components/PageSearch/PageSearch';
 // import { getFeaturedRoom } from '@/libs/apis';
 
@@ -14,7 +14,7 @@ const Home = async () => {
       <PageSearch />
       {/* <FeaturedRoom featuredRoom={featuredRoom} /> */}
       <Gallery />
-      {/* <NewsLetter /> */}
+      <NewsLetter />
     </>
   );
 };
