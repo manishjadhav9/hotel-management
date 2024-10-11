f5ef65c5639c36877a5b280bdd66f973f98f2d9f
Manish Jadhav <manishsj289@gmail.com>
2024-10-11 23:17:08 +0530

    created api for createBooking

--- src/libs/apis.ts
+++ src/libs/apis.ts
@@ -1,7 +1,8 @@
 
-import { Room } from "@/models/room";
+import { CreateBookingDto, Room } from "@/models/room";
 import sanityClient from "./sanity";
 import * as  queries from './sanityQueries';
+import axios from "axios";
 
 export async function getFeaturedRoom(){
   const result = await sanityClient.fetch<Room>(
@@ -32,4 +33,41 @@ export async function getRoom(slug: string) {
   return result;
 }
 
+export const createBooking = async ({
+  adults,
+  checkinDate,
+  checkoutDate,
+  children,
+  discount,
+  hotelRoom,
+  numberOfDays,
+  totalPrice,
+  user,
+}: CreateBookingDto) => {
+  const mutation = {
+    mutations: [
+      {
+        create: {
+          _type: 'booking',
+          user: { _type: 'reference', _ref: user },
+          hotelRoom: { _type: 'reference', _ref: hotelRoom },
+          checkinDate,
+          checkoutDate,
+          numberOfDays,
+          adults,
+          children,
+          totalPrice,
+          discount,
+        },
+      },
+    ],
+  };
 
+  const { data } = await axios.post(
+    `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-20-21/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
+    mutation,
+    {headers: {Authorization: `Bearer ${process.env.SANITY_STUDIO_TOKEN}`}}
+  );
+
+  return data;
+};
